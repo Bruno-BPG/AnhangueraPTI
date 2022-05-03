167e2eaaa90560de539b4043156b159d723c2a7d
Bruno Gomes <gomesbruno030@outlook.com>
2022-05-03 13:09:33 -0300

    03/05

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let Usuário = document.querySelector(".Usuário")
 let Senha = document.querySelector(".Senha")
 
-const pessoasobj = []
-
 let btnlogin = document.getElementById("login-button")
 btnlogin.addEventListener("click" , resultados)
 
@@ -15,49 +13,18 @@ let dadosDaPessoa = {
 }
 
 if(Usuário.value == ""){
-    gerarModal("digite seu Usuário")
-    exibirModal()
+    alert("digite seu Usuário")
     return "login falhou"
 }
 
 if(Senha.value == ""){
-    gerarModal("digite seu Senha")
-    exibirModal()
+    alert("digite seu Senha")
     return "falhou"
 }
 
 if(Usuário.value !== "" && Senha.value !== ""){
-    gerarModal("os dois campos foram prenchidos com sucesso")
-    exibirModal()
+    alert("os dois campos foram prenchidos com sucesso")
     return "login"
 }
 
-}
-
-const divConteinerModal = document.getElementById("divConteinerModal");
-
-function gerarModal(mensagem){
-    divConteinerModal.innerHTML = ""
-    const novobtn = document.createElement("button")
-    novobtn.classList = "botaoDeFechar"
-    novobtn.id = "btnFechar"
-    novobtn.innerText = "X"
-    const novoP = document.createElement("p")
-    novoP.classList = "msmDoMoldal"
-    novoP.innerText = mensagem
-
-    divConteinerModal.appendChild(novobtn);
-    divConteinerModal.appendChild(novoP);
-}
-
-function exibirModal() {
-    divConteinerModal.classList.remove("esconder");
-    divConteinerModal.classList.add("exibir");
-    divConteinerModal.addEventListener("click", esconderModal)
-}
-
-function esconderModal (){
-    divConteinerModal.classList.remove("exibir");
-    divConteinerModal.innerHTML = ""
-    divConteinerModal.classList.add("esconder");
 }
