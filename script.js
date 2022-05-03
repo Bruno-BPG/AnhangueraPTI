0c9c56d1aeae2f676e6f1158d35a7e0d5412e6ac
Bruno-BPG <91001161+Bruno-BPG@users.noreply.github.com>
2022-05-03 13:14:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 let Usuário = document.querySelector(".Usuário")
 let Senha = document.querySelector(".Senha")
-
 let btnlogin = document.getElementById("login-button")
 btnlogin.addEventListener("click" , resultados)
-
 function resultados (value){
 value.preventDefault()
-
 let dadosDaPessoa = {
     Usuário: Usuário.value,
     Senha: Senha.value,
 }
-
 if(Usuário.value == ""){
     alert("digite seu Usuário")
     return "login falhou"
 }
-
 if(Senha.value == ""){
     alert("digite seu Senha")
     return "falhou"
 }
-
 if(Usuário.value !== "" && Senha.value !== ""){
     alert("os dois campos foram prenchidos com sucesso")
     return "login"
-}
-
-}
+}}
